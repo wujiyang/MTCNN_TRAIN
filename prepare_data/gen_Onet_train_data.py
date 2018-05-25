01d13232125db731f3fefcc731774ab1d9fed8c9
邬小阳 <1441615466@qq.com>
2018-05-25 21:55:56 +0800

    generate onet training file

--- prepare_data/gen_Onet_train_data.py
+++ prepare_data/gen_Onet_train_data.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Mon May 21 22:17:05 2018
+Created on Fri May 25 20:12:13 2018
 
 @author: wujiyang
 """
+
 import sys
 sys.path.append("/home/wujiyang/FaceProjects/MTCNN_TRAIN")
 
@@ -22,26 +23,26 @@ from tools.utils import convert_to_square, IoU
 import config
 import tools.vision as vision
 
+def gen_onet_data(data_dir, anno_file, pnet_model_file, rnet_model_file, prefix_path='', use_cuda=True, vis=False):
+    
+    pnet, rnet, _ = create_mtcnn_net(p_model_path=pnet_model_file, r_model_path=rnet_model_file, use_cuda=use_cuda)
+    mtcnn_detector = MtcnnDetector(pnet=pnet, rnet=rnet, min_face_size=12)
 
-def gen_rnet_data(data_dir, anno_file, pnet_model_file, prefix_path='', use_cuda=True, vis=False):
-    # load the pnet and pnet_detector
-    
-    pnet, _, _ = create_mtcnn_net(p_model_path=pnet_model_file, use_cuda=use_cuda)
-    mtcnn_detector = MtcnnDetector(pnet=pnet, min_face_size=12)
-    
     imagedb = ImageDB(anno_file, mode="test", prefix_path=prefix_path)
     imdb = imagedb.load_imdb()
-    image_reader = TestImageLoader(imdb, 1, False)
-    
+    image_reader = TestImageLoader(imdb,1,False)
+
     all_boxes = list()
     batch_idx = 0
     
     for databatch in image_reader:
         if batch_idx % 100 == 0:
-            print ("%d images done" % batch_idx)
+            print("%d images done" % batch_idx)
         im = databatch
         t = time.time()
-        boxes, boxes_align = mtcnn_detector.detect_pnet(im=im)
+        #detect an image by pnet and rnet
+        p_boxes, p_boxes_align = mtcnn_detector.detect_pnet(im=im)
+        boxes, boxes_align = mtcnn_detector.detect_rnet(im=im, dets=p_boxes_align)
         if boxes_align is None:
             all_boxes.append(np.array([]))
             batch_idx += 1
@@ -49,7 +50,7 @@ def gen_rnet_data(data_dir, anno_file, pnet_model_file, prefix_path='', use_cuda
         if vis:
             rgb_im = cv2.cvtColor(np.asarray(im), cv2.COLOR_BGR2RGB)
             vision.vis_two(rgb_im, boxes, boxes_align)
-            
+
         t1 = time.time() - t
         print 'time cost for image ', batch_idx, '/', image_reader.size, ': ', t1
         all_boxes.append(boxes_align)
@@ -59,54 +60,57 @@ def gen_rnet_data(data_dir, anno_file, pnet_model_file, prefix_path='', use_cuda
     if not os.path.exists(save_path):
         os.mkdir(save_path)
     
-    save_file = os.path.join(save_path, "pnet_detections_%d.pkl" % int(time.time()))
+    save_file = os.path.join(save_path, "rnet_detections_%d.pkl" % int(time.time()))
     
     with open(save_file, 'wb') as f:
         cPickle.dump(all_boxes, f, cPickle.HIGHEST_PROTOCOL)
     
-    
     #save_file = '/home/wujiyang/FaceProjects/MTCNN_TRAIN/model_store/pnet_detections_1527162101.pkl'
-    get_rnet_sample_data(data_dir, anno_file, save_file, prefix_path)
+    get_onet_sample_data(data_dir, anno_file, save_file, prefix_path)
     
-def get_rnet_sample_data(data_dir, anno_file, det_boxes_file, prefix_path):
     
-    neg_save_dir = os.path.join(data_dir, "24/negative")
-    pos_save_dir = os.path.join(data_dir, "24/positive")
-    part_save_dir = os.path.join(data_dir, "24/part")
+def get_onet_sample_data(data_dir, anno_file, det_boxs_file, prefix):
+    
+    neg_save_dir = os.path.join(data_dir, "48/negative")
+    pos_save_dir = os.path.join(data_dir, "48/positive")
+    part_save_dir = os.path.join(data_dir, "48/part")
 
     for dir_path in [neg_save_dir, pos_save_dir, part_save_dir]:
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
-    
+
     # load ground truth from annotation file
-    # format of each line: image/path [x1, y1, x2, y2] for each gt_box in this image
+    # format of each line: image/path [x1,y1,x2,y2] for each gt_box in this image
+
     with open(anno_file, 'r') as f:
         annotations = f.readlines()
-        
-    image_size = 24
+
+    image_size = 48
     im_idx_list = list()
     gt_boxes_list = list()
     num_of_images = len(annotations)
-    print ("processing %d images in total" % num_of_images)
+    print("processing %d images in total" % num_of_images)
+    
     for annotation in annotations:
-    #for i in range(10):
         annotation = annotation.strip().split(' ')
-        #annotation = annotations[i].strip().split(' ')
-        im_idx = os.path.join(prefix_path, annotation[0])
+        im_idx = os.path.join(prefix,annotation[0])
+
         boxes = map(float, annotation[1:])
         boxes = np.array(boxes, dtype=np.float32).reshape(-1, 4)
         im_idx_list.append(im_idx)
         gt_boxes_list.append(boxes)
-    
+
+
     save_path = config.ANNO_STORE_DIR
     if not os.path.exists(save_path):
         os.makedirs(save_path)
-        
+
     f1 = open(os.path.join(save_path, 'pos_%d.txt' % image_size), 'w')
     f2 = open(os.path.join(save_path, 'neg_%d.txt' % image_size), 'w')
     f3 = open(os.path.join(save_path, 'part_%d.txt' % image_size), 'w')
-    
-    det_handle = open(det_boxes_file, 'r')
+
+    det_handle = open(det_boxs_file, 'r')
+
     det_boxes = cPickle.load(det_handle)
     print(len(det_boxes), num_of_images)
     assert len(det_boxes) == num_of_images, "incorrect detections or ground truths"
@@ -125,7 +129,7 @@ def get_rnet_sample_data(data_dir, anno_file, det_boxes_file, prefix_path):
         img = cv2.imread(im_idx)
         dets = convert_to_square(dets)
         dets[:, 0:4] = np.round(dets[:, 0:4])
-        
+
         # each image have at most 50 neg_samples
         cur_n_idx = 0
         for box in dets:
@@ -133,15 +137,15 @@ def get_rnet_sample_data(data_dir, anno_file, det_boxes_file, prefix_path):
             width = x_right - x_left
             height = y_bottom - y_top
             # ignore box that is too small or beyond image border
-            if width < 20 or x_left <= 0 or y_top <= 0 or x_right >= img.shape[1] or y_bottom >= img.shape[0]:
+            if width < 20 or x_left < 0 or y_top < 0 or x_right > img.shape[1] - 1 or y_bottom > img.shape[0] - 1:
                 continue
             # compute intersection over union(IoU) between current box and all gt boxes
             Iou = IoU(box, gts)
-            cropped_im = img[y_top:y_bottom, x_left:x_right, :]
+            cropped_im = img[y_top:y_bottom + 1, x_left:x_right + 1, :]
             resized_im = cv2.resize(cropped_im, (image_size, image_size),
                                     interpolation=cv2.INTER_LINEAR)
+
             # save negative images and write label
-            
             if np.max(Iou) < 0.3:
                 # Iou with all gts must below 0.3
                 cur_n_idx += 1
@@ -176,32 +180,31 @@ def get_rnet_sample_data(data_dir, anno_file, det_boxes_file, prefix_path):
                     offset_x1, offset_y1, offset_x2, offset_y2))
                     cv2.imwrite(save_file, resized_im)
                     d_idx += 1
-                    
     f1.close()
     f2.close()
     f3.close()
     
-
+    
+    
 def model_store_path():
     return os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))+"/model_store"
-
-
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Test mtcnn',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('--dface_traindata_store', dest='traindata_store', help='dface train data temporary folder',
-                        default=config.TRAIN_DATA_DIR, type=str)
+    parser.add_argument('--face_traindata_store', dest='traindata_store', help='face train data temporary folder',
+                        default='../data/wider/', type=str)
     parser.add_argument('--anno_file', dest='annotation_file', help='wider face original annotation file',
                         default=os.path.join(config.ANNO_STORE_DIR,"wider_origin_anno.txt"), type=str)
     parser.add_argument('--pmodel_file', dest='pnet_model_file', help='PNet model file path',
-                        default='/home/wujiyang/FaceProjects/MTCNN_TRAIN/model_store/pnet_model_final.pt', type=str)
+                        default=os.path.join(config.MODLE_STORE_DIR,"pnet_model_final.pt"), type=str)
+    parser.add_argument('--rmodel_file', dest='rnet_model_file', help='RNet model file path',
+                        default=os.path.join(config.MODLE_STORE_DIR,"rnet_model_final.pt"), type=str)
     parser.add_argument('--gpu', dest='use_cuda', help='with gpu',
                         default=config.USE_CUDA, type=bool)
     parser.add_argument('--prefix_path', dest='prefix_path', help='annotation file image prefix root path',
                         default='/home/wujiyang/data/Widerface/WIDER_train/images', type=str)
-
 
     args = parser.parse_args()
     return args
@@ -210,9 +213,5 @@ def parse_args():
 
 if __name__ == '__main__':
     args = parse_args()
-    gen_rnet_data(args.traindata_store, args.annotation_file, args.pnet_model_file, args.prefix_path, args.use_cuda)
-        
-    
-    
-    
-        
+    gen_onet_data(args.traindata_store, args.annotation_file, args.pnet_model_file, args.rnet_model_file, args.prefix_path, args.use_cuda)
+
